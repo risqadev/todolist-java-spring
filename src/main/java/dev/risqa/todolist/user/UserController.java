1df76c30ced7cebd2bca33393d15f30b875955ad
Ricardo Camacho <rscamachodev@gmail.com>
2023-10-13 16:46:20 -0300

    aula 2

---
+++ src/main/java/dev/risqa/todolist/user/UserController.java
@@ -0,0 +1,28 @@
+package dev.risqa.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+  @Autowired
+  private IUserRepository userRepository;
+  
+  @PostMapping
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if (user != null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já cadastrado.");
+    }
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(this.userRepository.save(userModel));
+  }
+}
